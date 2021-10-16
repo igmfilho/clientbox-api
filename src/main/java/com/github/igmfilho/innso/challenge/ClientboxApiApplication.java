c0f067ee6af99cd401816fd3d75c9ba8247df00b
Ivan Filho <igmfilho@gmail.com>
2021-10-16 19:12:16 +0100

    [Spring initializr] first source code was generated by start.spring.io

---
+++ src/main/java/com/github/igmfilho/innso/challenge/ClientboxApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.igmfilho.innso.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientboxApiApplication.class, args);
+	}
+
+}
